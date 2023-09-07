0821f7b85f22dde9cd14312e5c35acfb14c19021
hoderron99 <144006957+hoderron99@users.noreply.github.com>
2023-09-07 14:07:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+//hamburger menu 
+window.onload = function () {
+    window.addEventListener('scroll', function (e) {
+        if (window.pageYOffset > 100) {
+            document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
+
+
+
+//jackpot money 
+let value = 1000000
+setInterval(() => {
+    value += 235;
+    odormeter.innerHTML = value;
+    console.log(id);
+}, 190)
+
+
+//jackpot money  desk
+{
+    let value = 1000000
+    setInterval(() => {
+        value += 235;
+        odormeterdesk.innerHTML = value;
+        console.log(id); //set each scope in this id, else wont work
+    }, 190)
+    
+}
+
+
+
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("logo");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("logo")
+    } else {
+        navbar.classList.remove("logo");
+    }
+}
+
+
+
+//slides
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("myslide");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+
+
+
+
+
